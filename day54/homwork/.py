d1cb821831dba9377728136287003b2b240e7188
tomaomanashvili <tomaomanashvili6@gmail.com>
2024-08-13 09:41:33 +0200

    added day 54 54 55 55

---
+++ day54/homwork/.py
@@ -0,0 +1,39 @@
+
+def add_one(number):
+    return number + 1
+
+
+def check_greater_than_ten(number):
+    if number > 10:
+        return "მაგარია!"
+    else:
+        return "არ არის მაგარი :("
+
+
+def find_smaller_number(num1, num2):
+    return min(num1, num2)
+
+
+def get_text_length(text):
+    return len(text)
+
+
+def sum_two_numbers(num1, num2):
+    return num1 + num2
+
+
+def check_positive_or_negative(number):
+    if number > 0:
+        return "დადებითი"
+    elif number < 0:
+        return "უარყოფითი"
+    else:
+        return "ნულოვანი"
+
+
+def is_even(number):
+    return number % 2 == 0
+
+
+def sum_plus_five(num1, num2):
+    return num1 + num2 + 5
---
+++ day55/homwork/.py
@@ -0,0 +1,55 @@
+def add_one():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    return num + 1
+def is_greater_than_ten():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    if num > 10:
+        return "მაგარია!"
+    else:
+        return "არ არის 10-ზე მეტი."
+def find_min():
+    num1 = int(input("შემოატანეთ პირველი რიცხვი: "))
+    num2 = int(input("შემოატანეთ მეორე რიცხვი: "))
+    return min(num1, num2)
+def text_length():
+    text = input("შემოატანეთ ტექსტი: ")
+    return len(text)
+def sum_two_numbers():
+    num1 = int(input("შემოატანეთ პირველი რიცხვი: "))
+    num2 = int(input("შემოატანეთ მეორე რიცხვი: "))
+    return num1 + num2
+def sum_two_numbers():
+    num1 = int(input("შემოატანეთ პირველი რიცხვი: "))
+    num2 = int(input("შემოატანეთ მეორე რიცხვი: "))
+    return num1 + num2
+def check_sign():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    if num > 0:
+        return "დადებითი"
+    elif num < 0:
+        return "უარყოფითი"
+    else:
+        return "ნეიტრალური"  
+def check_sign():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    if num > 0:
+        return "დადებითი"
+    elif num < 0:
+        return "უარყოფითი"
+    else:
+        return "ნეიტრალური"  
+def check_sign():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    if num > 0:
+        return "დადებითი"
+    elif num < 0:
+        return "უარყოფითი"
+    else:
+        return "ნეიტრალური"  
+def is_even():
+    num = int(input("შემოატანეთ რიცხვი: "))
+    return num % 2 == 0
+def sum_and_add_five():
+    num1 = int(input("შემოატანეთ პირველი რიცხვი: "))
+    num2 = int(input("შემოატანეთ მეორე რიცხვი: "))
+    return num1 + num2 + 5
